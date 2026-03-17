8a76e3468240876b5d460ce14d5e6132d5b463dc
luigiPinna <luigipinna3@gmail.com>
2026-03-17 17:12:20 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/rent-template",
+  assetPrefix: "/rent-template",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
